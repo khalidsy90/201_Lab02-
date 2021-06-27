23d9183e66dcd446ea8221066aa6cafb15c90880
khalidsy90 <khalidhamedi3@gmail.com>
2021-06-27 03:27:11 +0300

    fix abdullah comments

--- js/app.js
+++ js/app.js
@@ -16,8 +16,6 @@ let btn1=document.getElementById("btn1")
 
     let question =['Did I graduate from Yarmouk University?','Have I been studying web developmen since 2009?','Am I a Sitecore Certified Professional Web Developer ?','Do I study chemistry in my spare time?','Do I have a desire to quit my profession?']
     let q=new Array()
-    let num_yes=0
-    let num_no=0
     let answers=['no','yes','yes','no','no']
 
     function lab02()
@@ -25,30 +23,42 @@ let btn1=document.getElementById("btn1")
         for (let i = 0; i < question.length; i++) 
         {
             let prom=prompt(question[i]).toLowerCase()
-            if (prom == 'y' || prom =='yes')
+            console.log(prom);
+            if(prom == 'y' || prom == 'yes' || prom == 'n' || prom == 'no' )
             {
-                q.push('yes')            
-            }
-            else if(prom =='n' || 'no')
-            {
-                q.push("no")
+                if (prom === 'y' || prom ==='yes')
+                {
+                    q.push('yes')            
+                }
+                // eslint-disable-next-line no-constant-condition
+                else if(prom === 'n' || 'no')
+                {
+                    q.push("no")
+                }
+                else
+                {
+                    alert("You Entered Something Wrong \n The website will reload to try again")
+                    window.location.reload()
+                    break
+                }
+    
+                if(q[i]===answers[i])
+                {
+                    score++
+                    alert("Your answer is correct")
+                }
+                else
+                {
+                    alert("Your answer is Wrong")
+                }     
             }
             else
             {
-                alert("You Entered Something Wrong \n The website will reload to try again")
+                alert('You entered an invalid answer \n I ll give you a chance to try again \n The browser will update again to give you a try')
                 window.location.reload()
                 break
             }
-
-            if(q[i]==answers[i])
-            {
-                score++
-                alert("Your answer is correct")
-            }
-            else
-            {
-                alert("Your answer is Wrong")
-            }
+           
         }
     }
 function lab03()
@@ -58,8 +68,6 @@ alert("Let's play a nice game")
 alert("Can you guess what my lucky number is ?")
 alert("I'll give you 4 tries to guess")
 alert("I'll give you a hint \n The number is greater than 10 and less than 20")
-
-let luckyNum=11
 
 for (let i = 0; i < 4; i++) 
 {
@@ -74,7 +82,7 @@ for (let i = 0; i < 4; i++)
     {
         alert("too high")
     }
-    else if(num == 0)
+    else if(num === 0)
     {
         alert("Gosh \n You guessed the right number \n It's already 11")  
         score++ 
@@ -84,14 +92,12 @@ for (let i = 0; i < 4; i++)
     {
         alert("You seem to have forgotten that the number is greater than 10")
         alert("I'll give you another try")
-        window.location.reload()
-        break;
+        i-=1
     }
     else
     {
-        alert("You Need to add jus a number")
-        window.location.reload()
-        break;
+        alert("You Need to add just a number")
+        i-=1
     }
 }
 alert("My luck number is 11")
@@ -110,7 +116,7 @@ for ( y = 0; y < 6; y++)
    let meal=prompt(`Food Number ${y+1}`).toLowerCase()
    for (let x = 0; x < food.length; x++) 
    {
-        if(meal == food[x])
+        if(meal === food[x])
         {
             alert("Right Food")
             score++
@@ -119,7 +125,7 @@ for ( y = 0; y < 6; y++)
             break;
         }  
    }  
-   if(flag == false) alert("Wrong Food")  
+   if(flag === false) alert("Wrong Food")  
 }
 alert("The Food are :mansaf,falafel,maqluba,shawarma,fattet hummus ")
 alert(`Your Total score is ${score}`)
